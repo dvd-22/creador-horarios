fd46c50a569008f44074a1a318433c07ba91d356
Dvd <dvdnet22@gmail.com>
2024-12-26 01:57:19 -0600

    Added github actions for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/favicon.svg'
 import './App.css'
 import ScheduleViewer from './components/ScheduleViewer'
 import ScheduleCreator from './components/ScheduleCreator';
